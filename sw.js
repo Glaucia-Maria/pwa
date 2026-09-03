5638474ac3c49e5b90a574114f901795d9b13c5d
Glaucia-Maria <glauciamariagnf2@gmail.com>
2026-09-03 19:00:46 -0300

    aaaaaaaaaaaaa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const expectedCaches = ['static-v7'];
+const CACHE_NAME = 'static-v7';
 
 const arquivos = [
     './',
@@ -7,7 +7,6 @@ const arquivos = [
     './script.js',
     './sw.js',
     './img/solar_system.jpg',
-    './img/solar_system2.jpg',
     './img/mercurio.jpg',
     './img/venus.jpg',
     './img/terra.jpg',
@@ -20,40 +19,48 @@ const arquivos = [
 
 self.addEventListener('install', event => {
     console.log('V7 instalando...');
+
     event.waitUntil(
-        caches.open('static-v6')
+        caches.open(CACHE_NAME)
             .then(cache => {
+                console.log('Cache aberto!');
                 return cache.addAll(arquivos);
+            })
+            .then(() => {
+                console.log('V7 instalado!');
+                return self.skipWaiting();
             })
     );
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if(!expectedCaches.includes(key)) {
-                    return caches.delete(key);
-                }
+        caches.keys()
+            .then(keys => {
+                return Promise.all(
+                    keys.map(key => {
+                        if (key !== CACHE_NAME) {
+                            return caches.delete(key);
+                        }
+                    })
+                );
             })
-        )).then(() => {
-            console.log("V7 pronto para o fetch!")
-        })
-    )
+            .then(() => {
+                console.log('V7 ativado!');
+                return self.clients.claim();
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-    // DESAFIO:
-    // troca a imagem do Sistema Solar pela imagem alternativa
-    if (url.origin === location.origin && url.pathname === '/img/solar_system.jpg') {
-        event.respondWith(caches.match('/img/solar_system2.jpg')); return;
-    }
-
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return response || fetch(event.request);
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request);
             })
     );
 });
